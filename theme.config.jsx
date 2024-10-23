151de6cdd706c860ed946e1cd2eb2a452d8f88e9
Jim Chen <jimchen4214@gmail.com>
2024-10-23 13:33:10 +0800

    init

---
+++ theme.config.jsx
@@ -0,0 +1,20 @@
+export default {
+  logo: <span>Jim Chen's Blog</span>,
+  project: {
+    link: "https://github.com/jimchen2/jimchen.me",
+  },
+  feedback: {
+    content: "",
+    labels: "feedback",
+  },
+  editLink: {
+    content: "",
+  },
+  footer: {
+    content: (
+      <span>
+        © {new Date().getFullYear()} <a href="https://github.com/jimchen2">Jim Chen</a>. Built with Nextra <a href="https://github.com/shuding/nextra">Nextra</a>.
+      </span>
+    ),
+  },
+};
