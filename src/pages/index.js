7c93709839801e2ccbf9aca193c4396d2923e5ab
Jim Chen <jimchen4214@gmail.com>
2025-04-01 00:00:32 +0800

    Updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
-import BlogPage from "./page/[page]";
+import BlogPage, { getServerSideProps as getBlogPageServerSideProps } from './page/[page]';
+
+export async function getServerSideProps(context) {
+  return getBlogPageServerSideProps({ ...context, params: { page: '1' } });
+}
 
 export default BlogPage;
