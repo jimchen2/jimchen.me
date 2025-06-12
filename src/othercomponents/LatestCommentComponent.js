446aca8ffb11149e629ce89249b1d0d4c05422ed
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2025-06-12 22:12:42 +0800

    Update LatestCommentComponent.js

--- src/othercomponents/LatestCommentComponent.js
+++ src/othercomponents/LatestCommentComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link'; // 1. Import the Link component
 import GetComments from "@/comment/getcomments";
 import { CommentsProvider } from "@/comment/commentscontext";
 
@@ -10,6 +11,13 @@ const Msg = () => {
       <CommentsProvider>
         <GetComments blogid="0" showName paddl="0" paddr="0" limit="3" />
       </CommentsProvider>
+
+      {/* 2. Add the "Read more" link after the comments */}
+      <div style={{ marginTop: '1rem', textAlign: 'right' }}>
+        <Link href="/comments" style={{ textDecoration: 'underline', cursor: 'pointer', color: 'blue' }}>
+          See all comments...
+        </Link>
+      </div>
     </div>
   );
 };
