285f210c836ce9f9766d728a4e63c1773b643ef1
Jim Chen <jimchen4214@gmail.com>
2025-10-01 23:42:14 +0800

    Updates

--- src/blogpreview/othercomponents/AboutComponent.js
+++ src/blogpreview/othercomponents/AboutComponent.js
@@ -2,33 +2,80 @@
 
 import React from "react";
 import Link from "next/link";
-import { useTranslation } from 'next-i18next';
-import { FaGithub, FaWeixin, FaTelegram, FaLinkedin, FaEnvelope, FaGoodreads, FaYoutube, FaWhatsapp } from "react-icons/fa";
-import { SiInternetarchive, SiOpenstreetmap, SiTampermonkey, SiHuggingface, SiBitwarden } from "react-icons/si";
-
+import { useTranslation } from "next-i18next";
+import {
+  FaGithub,
+  FaWeixin,
+  FaTelegram,
+  FaLinkedin,
+  FaEnvelope,
+  FaGoodreads,
+  FaYoutube,
+} from "react-icons/fa";
+import { SiInternetarchive, SiTampermonkey } from "react-icons/si";
 
 function AboutComponent() {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
 
   // Define links inside the component to access the 't' function
   const profileLinks = [
-    { href: "https://github.com/jimchen2", Icon: FaGithub, alt: t('about.profile.github_alt'), name: t('about.linkNames.github') },
-    { href: "https://www.linkedin.com/in/jim-chen-588002255/", Icon: FaLinkedin, alt: t('about.profile.linkedin_alt'), name: t('about.linkNames.linkedin') },
-    { href: "https://www.youtube.com/@jimchen4214", Icon: FaYoutube, alt: t('about.profile.youtube_alt'), name: t('about.linkNames.youtube') },
-  ];
-
-  const socialLinks = [
-    { href: "mailto:jimchen4214@gmail.com", Icon: FaEnvelope, alt: t('about.contact.email_alt'), name: t('about.linkNames.email') },
-    { href: "https://t.me/Jimchen4214", Icon: FaTelegram, alt: t('about.contact.telegram_alt'), name: t('about.linkNames.telegram') },
-    { href: "https://jimchen.me/weixin.jpg", Icon: FaWeixin, alt: t('about.contact.wechat_alt'), name: t('about.linkNames.wechat') },
+    {
+      href: "https://github.com/jimchen2",
+      Icon: FaGithub,
+      alt: t("about.profile.github_alt"),
+      name: t("about.linkNames.github"),
+    },
+    {
+      href: "https://www.linkedin.com/in/jim-chen-588002255/",
+      Icon: FaLinkedin,
+      alt: t("about.profile.linkedin_alt"),
+      name: t("about.linkNames.linkedin"),
+    },
+    {
+      href: "mailto:jimchen4214@gmail.com",
+      Icon: FaEnvelope,
+      alt: t("about.contact.email_alt"),
+      name: t("about.linkNames.email"),
+    },
+    {
+      href: "https://t.me/Jimchen4214",
+      Icon: FaTelegram,
+      alt: t("about.contact.telegram_alt"),
+      name: t("about.linkNames.telegram"),
+    },
+    {
+      href: "https://jimchen.me/weixin.jpg",
+      Icon: FaWeixin,
+      alt: t("about.contact.wechat_alt"),
+      name: t("about.linkNames.wechat"),
+    },
   ];
 
   const utilityLinks = [
-    { href: "https://archive.org/details/@j_c561", Icon: SiInternetarchive, alt: t('about.utility.archive_alt'), name: t('about.linkNames.archive') },
-    { href: "https://umap.openstreetmap.fr/en/user/jimchen2/", Icon: SiOpenstreetmap, alt: t('about.utility.umap_alt'), name: t('about.linkNames.umap') },
-    { href: "https://greasyfork.org/en/users/1430831-jimchen2", Icon: SiTampermonkey, alt: t('about.utility.greasyfork_alt'), name: t('about.linkNames.greasyfork') },
-    { href: "https://huggingface.co/jimchen2", Icon: SiHuggingface, alt: t('about.utility.huggingface_alt'), name: t('about.linkNames.huggingface') },
-    { href: "https://www.goodreads.com/user/show/154371677-jim-chen", Icon: FaGoodreads, alt: t('about.utility.goodreads_alt'), name: t('about.linkNames.goodreads') },
+    {
+      href: "https://www.youtube.com/@jimchen4214",
+      Icon: FaYoutube,
+      alt: t("about.profile.youtube_alt"),
+      name: t("about.linkNames.youtube"),
+    },
+    {
+      href: "https://archive.org/details/@j_c561",
+      Icon: SiInternetarchive,
+      alt: t("about.utility.archive_alt"),
+      name: t("about.linkNames.archive"),
+    },
+    {
+      href: "https://greasyfork.org/en/users/1430831-jimchen2",
+      Icon: SiTampermonkey,
+      alt: t("about.utility.greasyfork_alt"),
+      name: t("about.linkNames.greasyfork"),
+    },
+    {
+      href: "https://www.goodreads.com/user/show/154371677-jim-chen",
+      Icon: FaGoodreads,
+      alt: t("about.utility.goodreads_alt"),
+      name: t("about.linkNames.goodreads"),
+    },
   ];
 
   const renderLinkSection = (title, links) => (
@@ -42,14 +89,14 @@ function AboutComponent() {
 
           // Style for each line item in the list
           const linkItemStyle = {
-            display: 'flex',
-            alignItems: 'center',
-            padding: '10px',
-            borderRadius: '5px',
-            textDecoration: 'none',
-            color: 'inherit',
-            transition: 'background-color 0.2s ease-in-out',
-            marginBottom: '5px',
+            display: "flex",
+            alignItems: "center",
+            padding: "10px",
+            borderRadius: "5px",
+            textDecoration: "none",
+            color: "inherit",
+            transition: "background-color 0.2s ease-in-out",
+            marginBottom: "5px",
           };
 
           // Style for the icon
@@ -73,7 +120,7 @@ function AboutComponent() {
 
           if (isImageDisplay) {
             return (
-              <div key={link.href} title={link.alt} style={{...linkItemStyle, cursor: 'pointer'}}>
+              <div key={link.href} title={link.alt} style={{ ...linkItemStyle, cursor: "pointer" }}>
                 {content}
               </div>
             );
@@ -100,9 +147,8 @@ function AboutComponent() {
 
   return (
     <div style={{ fontFamily: "Ubuntu, sans-serif", paddingTop: "1rem" }}>
-      {renderLinkSection(t('about.profile.title'), profileLinks)}
-      {renderLinkSection(t('about.contact.title'), socialLinks)}
-      {renderLinkSection(t('about.utility.title'), utilityLinks)}
+      {renderLinkSection(t("about.profile.title"), profileLinks)}
+      {renderLinkSection(t("about.utility.title"), utilityLinks)}
     </div>
   );
 }
