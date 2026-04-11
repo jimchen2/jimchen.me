9eb2272eca1e84c57d617f4fe6a531876d1798ed
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2026-04-11 19:04:52 +0800

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ const links = {
   github: "https://github.com/jimchen2",
   linkedin: "https://www.linkedin.com/in/jim-chen-588002255/",
   telegram: "https://t.me/Jimchen4214",
-  instagram: "https://www.instagram.com/jimchen_1",
+  instagram: "https://www.instagram.com/jimchenalaska/",
   wechat: "https://jimchen.me/weixin.jpg",
   youtube: "https://www.youtube.com/@jimchen4214",
   tiktok: "https://www.tiktok.com/@jimchen.me",
